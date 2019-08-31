9fc9ce5731c99d15d41d218a000a31e3f7038ec9
Rossano-DAngelo <rossano.dangelo@ibm.com>
2019-08-31 22:16:59 +0100

    Setting up for demo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Chart } from 'react-google-charts';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataLoadingStatus: 'loading',
+      chartData: []
+    };
+  }
+  componentDidMount = async () => {
+    const response = await fetch('https://api.exchangeratesapi.io/latest?symbols=USD,GBP,CAD,NZD');
+    const json = await response.json();
+    const rateCurrencyNames = Object.keys(json.rates);
+    const rateCurrencyValues = Object.values(json.rates);
+    const chartData = [['Currency Name', 'Currency Rate']];
+    for (let i = 0; i < rateCurrencyNames.length; i += 1) {
+      chartData.push([rateCurrencyNames[i], rateCurrencyValues[i]])
+    }
+    this.setState({
+      dataLoadingStatus: 'ready',
+      chartData: chartData,
+    });
+  }
+  render () {
+    return (
+      this.state.dataLoadingStatus === 'ready' ? (
+        <Chart
+          chartType="BarChart"
+          data={this.state.chartData}
+          options={{
+            chartArea: {
+              width: '50%',
+            },
+            title: 'EUR Price',
+          }}
+          rootProps={{ 'data-testid': '1' }}
+        />
+      ) : (
+        <div>Fetching data from API</div>
+      )
+    );
+  }
 }
 
 export default App;
